3ac1ee7200ca7b24c4db607fb9eb8d6d7fd2b419
Replit user <>
2023-05-12 13:31:34 +0000

    new try

--- heap.c
+++ heap.c
@@ -54,8 +54,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
     aux = padre; 
     
-    pq->heapArray[nuevoD]= aux;
-    printf("%i", aux.priority); 
+    //pq->heapArray[nuevoD]= aux; 
     
     pq->heapArray[(nuevoD-1)/2] = pq->heapArray[nuevoD]; 
     pq->heapArray[nuevoD] = aux; 
